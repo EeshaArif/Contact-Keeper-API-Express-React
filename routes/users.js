200734c2661cd040cdc4d5227600aaffbc760271
EeshaArif <eeshaarif@gmail.com>
2020-05-29 08:23:04 +0500

    Hashed Password-User-In-DataBase

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
 // @route    POST api/users
@@ -15,13 +16,32 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const { name, email, password } = req.body;
 
-    res.send("passed");
+    try {
+      let user = await User.findOne({ email: email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      res.send("User Saved");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("server error");
+    }
   }
 );
 module.exports = router;
